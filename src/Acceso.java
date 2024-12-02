f0fe07c39c8283b691b262fd05a8023ba01ef0ab
alejandroserrano11 <alejandro.s383963@cesurformacion.com>
2024-12-02 14:09:11 +0100

    accesoDatosUd2 actu

--- src/Acceso.java
+++ src/Acceso.java
@@ -6,10 +6,15 @@ import java.sql.SQLException;
 import java.sql.Statement;
 
 public class Acceso {
+
+    // atributos
+
     private Connection conexion;
     private Statement statement;
     private ResultSet resultSet;
     ResultSetMetaData metaDatos;
+
+    // constructor
 
     public Acceso() {
         this.conexion = null;
@@ -17,17 +22,23 @@ public class Acceso {
         this.resultSet = null;
     }
 
+    // método para establecer la conexión con la base de datos
+
     public void conectar() {
+
         try {
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.jdbc.Driver"); // carga del driver JDBC
         } catch (ClassNotFoundException e) {
             System.out.println(e.getMessage());
         }
 
         // establecer los datos para la conexion
+
         String url = "jdbc:mysql://localhost:3306/aad";
         String usuario = "root";
         String contrasena = "";
+
+        // conectar y crear el statement
 
         try {
             conexion = DriverManager.getConnection(url, usuario, contrasena);
@@ -38,7 +49,12 @@ public class Acceso {
         }
     }
 
+    // método para cerrar los recursos abiertos
+
     public void desconectar() {
+
+        // cierra el statement
+
         if (statement != null) {
             try {
                 statement.close();
@@ -46,6 +62,8 @@ public class Acceso {
                 System.out.println(e.getMessage());
             }
         }
+
+        // cierra la conexión
 
         if (conexion != null) {
             try {
@@ -56,9 +74,14 @@ public class Acceso {
         }
     }
 
+    // método para ejecutar una consulta SQL y mostrar los resultados
+
     public void realizarConsulta(String consulta) {
+
         try {
-            resultSet = statement.executeQuery(consulta);
+            resultSet = statement.executeQuery(consulta); // ejecuta la consulta y almacena los datos en resultSet
+
+            // procesamiento de los resultados
             boolean seguir = resultSet.next();
             while (seguir) {
                 System.out.println(resultSet.getInt(1) + " " +
@@ -74,8 +97,13 @@ public class Acceso {
         }
     }
 
+    // método para mostrar información adicional sobre los metadatos de los resultados
+
     public void realizarConsultaFormateada(String consulta) {
         try {
+
+            // obtiene y muestra metadatos
+
             resultSet = statement.executeQuery(consulta);
             metaDatos = resultSet.getMetaData();
             System.out.println("Número de columnas: " + metaDatos.getColumnCount());
@@ -84,12 +112,15 @@ public class Acceso {
             System.out.println();
             System.out.println("Datos de la tabla " + metaDatos.getTableName(1) + " con columnas");
             System.out.println("-----------------------------------");
+
+            // muestra los nombres de las columnas
+
             for (int i = 1; i < metaDatos.getColumnCount() + 1; i++) {
                 System.out.print(metaDatos.getColumnName(i) + " ");
             }
 
             System.out.println();
-            realizarConsulta(consulta);
+            realizarConsulta(consulta); // muestra los datos de las filas
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
--- src/App.java
+++ src/App.java
@@ -1,18 +1,17 @@
 public class App {
     public static void main(String[] args) throws Exception {
+
+        // establezco la conexión con la base de datos
         Acceso miAcceso = new Acceso();
         miAcceso.conectar();
-        miAcceso.realizarConsulta("SELECT * FROM ALUMNOS");
+
+        miAcceso.realizarConsulta("SELECT * FROM ALUMNOS"); // muestra los datos de la tabla alumnos
         System.out.println();
         System.out.println("***********************************");
         System.out.println();
-        miAcceso.realizarConsultaFormateada("SELECT * FROM ALUMNOS");
-        
-        
-        
+        miAcceso.realizarConsultaFormateada("SELECT * FROM ALUMNOS"); // muestra metadatos y datos de la tabla
 
-
-
+        // libera todos los recursos
         miAcceso.desconectar();
     }
 }
